47543ee10ff914da64e408d7cc3466920ff30d74
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-09-23 21:31:48 -0300

    feat(js): comportamento para simbolo 'v' ao descer scroll

---
+++ app/static/js/behaviors.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    const symbol = document.querySelector('.symbol');
+
+    window.addEventListener('scroll', function() {
+        let scrollPos = window.scrollY;
+
+        let distanciaFade = 50;
+
+        let novaOpacidade = 1 - (scrollPos / distanciaFade);
+
+        // Garante que a opacidade não seja menor que 0
+        if (novaOpacidade < 0) {
+            novaOpacidade = 0;
+        }
+
+        // Garante que a opacidade não seja maior que 1
+        if (novaOpacidade > 1) {
+            novaOpacidade = 1;
+        }
+
+        symbol.style.opacity = novaOpacidade;
+    });
+});
